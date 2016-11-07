3ce4ade4f72e24103eb68191aa3b8e9573a87771
mannma <mannma@sina.com>
2016-11-07 14:50:22 +0800

    Add files via upload

--- PassWordDict.py
+++ PassWordDict.py
@@ -1,14 +1,13 @@
 import crypt
 def testPass(cryptPass):
 	salt=cryptPass[0:2]
-
-dictFile=open('dictionary.txt','r')
-for word in dictFile.readlines():
-	word=word.strip('\n')
-	cryptWord=crypt.crypt(word,salt)
-	if(cryptWord == cryptPass):
-		print "[+] Found Password : " +word+"\n"
-		return
+	dictFile=open('dictionary.txt','r')
+	for word in dictFile.readlines():
+		word=word.strip('\n')
+		cryptWord=crypt.crypt(word,salt)
+		if(cryptWord == cryptPass):
+			print "[+] Found Password : " +word+"\n"
+			return
 	print "[-] Password Not Found. \n"
 	return
 
